b2a7feaa060bf9773f900829c78c37f9eaf3803c
Фаль Александр Евгеньевич <aleksandr.fal@gt-m.ru>
2023-05-25 16:41:17 +0300

    Свагер - OAuth 2.0

--- bootstrap/src/main/java/com/example/modulith/demo/bootstrap/SpringdocConfiguration.java
+++ bootstrap/src/main/java/com/example/modulith/demo/bootstrap/SpringdocConfiguration.java
@@ -5,11 +5,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 
 @Configuration
-@SecurityScheme(
-    name = "Bearer Auth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer"
+@SecuritySchemes(
+    {
+        @SecurityScheme(
+            name = "Bearer Auth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer"
+        ), @SecurityScheme(
+        name = "OAuth 2.0", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(
+        password = @OAuthFlow(
+            tokenUrl = "http://localhost:8180/realms/modulith/protocol/openid-connect/token",
+            refreshUrl = "http://localhost:8180/realms/modulith/protocol/openid-connect/token"
+        )
+    )
+    )
+    }
 )
 public class SpringdocConfiguration {
     @Bean
